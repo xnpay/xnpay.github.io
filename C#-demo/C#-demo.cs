8e337b750cb1f8a552151cc9592ddeb96d96f862
rongpay <58580843+rongpay@users.noreply.github.com>
2019-12-11 20:53:05 +0800

    查询, 回调验签demo

--- C#-demo/C#-demo.cs
+++ C#-demo/C#-demo.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Web;
 using System.Net;
 using BCrypt.Net;
+using System.IO;
 public class Demo
 {
 	/**
@@ -18,11 +19,13 @@ public class Demo
 	public static void Main()
 	{
 		//创建订单
-		createOrder();
+		//createOrder();
 		
 		//验签
-		Console.WriteLine(checkHash("J9yMyqsbweqKqGk9cqQ/ESv1BdiUDVLXxB5BbF2eck8=", "$2a$10$6qizWcRhDOraGeIPWWE7M.UL0CvvlUxJQwpoZpCq/wD2w9GNfDhd2"));
-	
+		//Console.WriteLine(checkHash("J9yMyqsbweqKqGk9cqQ/ESv1BdiUDVLXxB5BbF2eck8=", "$2a$10$6qizWcRhDOraGeIPWWE7M.UL0CvvlUxJQwpoZpCq/wD2w9GNfDhd2"));
+		
+		//查询订单
+		queryOrder();
 	}
 	
 	
@@ -42,16 +45,94 @@ public class Demo
 		parameters.Add("notifyUrl", "https://www.baidu.com/");
 		parameters.Add("returnUrl", "https://www.baidu.com/");
 		
+		//拼接参数
 		String paramsOrder = getParamsOrder(parameters);
 		Console.WriteLine(paramsOrder);
-		
+		//签名
 		String signStr = sign(API_KEY, paramsOrder);
+		//UrlEncode
+		signStr = WebUtility.UrlEncode(signStr);
 		
 		//请求地址
 		String host = "网关地址/pay-order/#/?" + paramsOrder + "&sign=" + signStr;
 		Console.WriteLine(host);
-
 	}
+	
+	
+	/*
+	 * 查询订单
+	 * 回调验签demo
+	 * (仅供参考, 实际使用自行修改)
+	 */
+	public static void queryOrder()
+	{
+		String host = "网关地址/any-pay/open/order/query";
+		String ts = ((DateTime.Now.ToUniversalTime().Ticks - 621355968000000000) / 10000000) + "";
+		SortedDictionary<string, string> parameters = new SortedDictionary<string, string>();
+		parameters.Add("orderNo", "1576061857");
+		parameters.Add("merchantNo", "20191204192421307122140114");
+		parameters.Add("ts", ts);
+		
+		//拼接参数
+		String paramsOrder = getParamsOrder(parameters);
+		//签名
+		String signStr = sign(API_KEY, paramsOrder);
+		parameters.Add("sign", signStr);
+		Console.WriteLine(signStr);
+		
+		//此处为json 数据, 实际使用引入json库 对parameters对象序列化即可
+		String jsonData = "{\"orderNo\":\"1576061857\",\"merchantNo\":\"20191204192421307122140114\",\"ts\":\"" + ts + "\",\"sign\":\"" + signStr + "\"}";
+		
+		var request = (HttpWebRequest)WebRequest.Create(host);
+		request.Method = "POST";
+		request.ContentType = "application/json;charset=UTF-8";
+		var byteData = Encoding.UTF8.GetBytes(jsonData);
+		var length = byteData.Length;
+		request.ContentLength = length;
+		var writer = request.GetRequestStream();
+		writer.Write(byteData, 0, length);
+		writer.Close();
+		var response = (HttpWebResponse)request.GetResponse();
+		//查询结果
+		var responseString = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding("utf-8")).ReadToEnd();
+		Console.WriteLine(responseString);
+	}
+	
+	
+	/*
+	 *	回调验签 与 查询验签
+	 *	模拟代码, 仅供参考, 实际使用自行修改
+	 */ 
+	public static void callbackVerify()
+	{
+		//模拟查询响应结果 与回调参数验签
+		String result = "{\"amount\":100,\"orderNo\":\"1576061857\",\"merchantNo\":\"20191204192421307122140114\",\"ts\":1576061857,\"payNo\":\"20191211185756217129352808\",\"payStatus\":-10,\"payMode\":\"100001\",\"orderStatus\":-40,\"payTime\":null,\"sign\":\"$2a$10$PBDOrNdRIYvWnL8msEjMSeX/4PXSYOjaVkvjc/2QLq2Vc558TjFES\"}";
+		//此处模拟回调(查询)响应参数, 实际使用引入json库序列化即可
+		SortedDictionary<string, string> resultDictionary = new SortedDictionary<string, string>();
+		resultDictionary.Add("amount", "100");
+		resultDictionary.Add("orderNo", "1576061857");
+		resultDictionary.Add("merchantNo", "20191204192421307122140114");
+		resultDictionary.Add("ts", "1576061857");
+		resultDictionary.Add("payNo", "20191211185756217129352808");
+		resultDictionary.Add("payStatus", "-10");
+		resultDictionary.Add("payMode", "100001");
+		resultDictionary.Add("orderStatus", "-40");
+		resultDictionary.Add("payTime", null);
+		resultDictionary.Add("sign", "$2a$10$PBDOrNdRIYvWnL8msEjMSeX/4PXSYOjaVkvjc/2QLq2Vc558TjFES");
+		
+		//拼接参数
+		String resultParam = getParamsOrder(resultDictionary);
+		//获取到 Base64(SHA-256(apiKey + paramsOrder + apiKey)) 字符串
+		String resultBase64Str = sha256AndBase64(API_KEY + resultParam + API_KEY);
+		
+		//校验
+		Console.WriteLine(checkHash(resultBase64Str, resultDictionary["sign"]));
+		
+	}
+	
+	
+	
+	
 	
 	
 	
@@ -66,7 +147,17 @@ public class Demo
 	}
 	
 	
-	
+	/*
+	 *	先sha256后再执行base64
+	 *	Base64(SHA-256(apiKey + paramsOrder + apiKey))
+	 */
+	public static String sha256AndBase64(String source)
+	{
+		//sha256
+		byte[] tmpByte = new SHA256Managed().ComputeHash(Encoding.UTF8.GetBytes(source));
+   		 //base64
+		return Convert.ToBase64String(tmpByte);
+	}
 	
 	/*
 	 * 签名
@@ -74,10 +165,11 @@ public class Demo
 	public static String sign(String apiKey, String paramsOrder)
 	{
 		String signStr = apiKey + paramsOrder + apiKey;
-		byte[] tmpByte = new SHA256Managed().ComputeHash(Encoding.UTF8.GetBytes(signStr));
-		String base64Str = Convert.ToBase64String(tmpByte);
-		//UrlEncode
-		return WebUtility.UrlEncode(BCrypt.Net.BCrypt.HashPassword(base64Str));
+   		 //sha256 &&  base64
+		String base64Str = sha256AndBase64(signStr);
+		//BCrypt
+		String bcrypt = BCrypt.Net.BCrypt.HashPassword(base64Str);
+		return bcrypt;
 	}
 
 	/*
@@ -95,7 +187,7 @@ public class Demo
 		{
 			string key = dem.Current.Key;
 			string value = dem.Current.Value;
-			if (!string.IsNullOrEmpty(key) && !string.IsNullOrEmpty(value))
+			if (!string.IsNullOrEmpty(key) && !string.IsNullOrEmpty(value) && !"sign".Equals(key))
 			{
 				query.Append(key).Append("=").Append(WebUtility.UrlEncode(value)).Append("&");				
 			}
